9ab5c92abd50fbaa71a24d2e5b07ba1c59655340
Abhilash_43 <67199624+abhilash43@users.noreply.github.com>
2020-07-03 09:23:15 +0530

    Create Number of characters in a stings.py

---
+++ Number of characters in a stings.py
@@ -0,0 +1,8 @@
+#Number of characters in a stings.py
+    str1 = input("Please Enter your Own String : ")
+total = 0
+ 
+for i in str1:
+    total = total + 1
+ 
+print("Total Number of Characters in this String = ", total)
